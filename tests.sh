2faf4244cc69b6a0e4823b900fb4fc9ce1134221
I0x0I <ivan.i0x0i@gmail.com>
2025-04-10 09:46:29 -0500

    Updated Autograder

--- tests.sh
+++ tests.sh
@@ -27,25 +27,108 @@ SCORE=0
 echo -e "\nStart testing"
 remake
 echo -e "\nTesting :: Compilation\n"
-echo -e "  ${GREEN}Test One Passed${NC}"
+echo -e "  ${GREEN}Test #1 Passed${NC}"
 SCORE=$(($SCORE+10))
 
+
+
 remake
-echo -e "\nTesting :: Correct output for exiting normally - ./main -n 0\n"
+echo -e "\nTesting :: Correct ls -l when exiting normally - ./main -n 0\n"
+echo -e "n\nr\n"|gdb --args ./main -n 0 > test_normal_log.txt
+sleep 6 #in case no valid wait() call in main.cpp
+child_pid=`grep -Pzo "(?<=child process )[0-9]+" test_normal_log.txt`
+parent_pid=`grep -Pzo "(?<=\(process )[0-9]+" test_normal_log.txt`
 RES=$(. ./test-files/test_normal.txt)
-if ./main -n 0 2>/dev/null | grep -F -- "${RES}" | wc -l | grep -qE "[7-9]|1[0-9]"; then
-    echo -e "  ${GREEN}Test Two Passed${NC}"
-    SCORE=$(($SCORE+20))
+echo "$RES" > test_normal_reference.txt
+EXIT_MSG=$(echo "$RES"|tail -1)
+if  [ $(grep -o "`ls -l`" test_normal_log.txt|wc -l) -ge 1 ]; then
+    echo -e "  ${GREEN}Test #2 Passed${NC}"
+    SCORE=$(($SCORE+9))
+else
+    echo -e "  ${RED}Failed${NC}"
+fi
+echo -e "\nTesting :: Correct exit message when exiting normally - ./main -n 0\n"
+if  [ $(grep -o "${EXIT_MSG}" test_normal_log.txt|wc -l) -ge 1 ]; then
+    echo -e "  ${GREEN}Test #3 Passed${NC}"
+else
+    echo -e "  ${RED}Failed${NC}"
+fi
+echo -e "\nTesting :: Correct parent PID when exiting normally - ./main -n 0\n"
+if  [ $(grep -o "$parent_pid" test_normal_log.txt|wc -l) -ge 2 ]; then
+    echo -e "  ${GREEN}Test #4 Passed${NC}"
+    SCORE=$(($SCORE+4))
+else
+    echo -e "  ${RED}Failed${NC}"
+fi
+echo -e "\nTesting :: Correct child PID when exiting normally - ./main -n 0\n"
+if  [ $(grep -o "$child_pid" test_normal_log.txt|wc -l) -ge 2 ]; then
+    echo -e "  ${GREEN}Test #5 Passed${NC}"
+    SCORE=$(($SCORE+4))
 else
     echo -e "  ${RED}Failed${NC}"
 fi
 
+
+
+
 remake
-echo -e "\nTesting :: Correct output for exiting with kill - ./main -n 1\n"
+echo -e "\nTesting :: Correct wait or waitpid\n"
+echo -e "n\nr\n"|gdb --args ./main -n 1 > test_kill_log.txt
+child_pid=`grep -Pzo "(?<=child process )[0-9]+" test_kill_log.txt`
+parent_pid=`grep -Pzo "(?<=\(process )[0-9]+" test_kill_log.txt`
 RES=$(. ./test-files/test_kill.txt)
-if ./main -n 1 2>/dev/null | grep -F -- "${RES}" | wc -l | grep -qE "[3-9]"; then
-    echo -e "  ${GREEN}Test Three Passed${NC}"
-    SCORE=$(($SCORE+20))
+echo "$RES" > test_kill_reference.txt
+EXIT_MSG=$(echo "$RES"|tail -1)
+if  [ $(grep -Pzo 'wait\((?!\s*NULL\s*)\s*\&*[a-zA-Z]+\s*\);' main.cpp|wc -w) -ge 1 ] ||
+    [ $(grep -Pzo 'waitpid\(.*,(?!\s*NULL\s*)\s*\&*[a-zA-Z]+\s*,.*\);' main.cpp|wc -w) -ge 1 ]; then
+    echo -e "  ${GREEN}Test #6 Passed${NC}"
+    SCORE=$(($SCORE+9))
+else
+    echo -e "  ${RED}Failed${NC}"
+fi
+echo -e "\nTesting :: Correct exit message when exiting with kill - ./main -n 1\n"
+if  [ $(grep -o "${EXIT_MSG}" test_kill_log.txt|wc -l) -ge 1 ]; then
+    echo -e "  ${GREEN}Test #7 Passed${NC}"
+else
+    echo -e "  ${RED}Failed${NC}"
+fi
+echo -e "\nTesting :: Correct parent PID when exiting with kill - ./main -n 1\n"
+if  [ $(grep -o "$parent_pid" test_kill_log.txt|wc -l) -ge 2 ]; then
+    echo -e "  ${GREEN}Test #8 Passed${NC}"
+    SCORE=$(($SCORE+4))
+else
+    echo -e "  ${RED}Failed${NC}"
+fi
+echo -e "\nTesting :: Correct child PID when exiting with kill - ./main -n 1\n"
+if  [ $(grep -o "$child_pid" test_kill_log.txt|wc -l) -ge 2 ]; then
+    echo -e "  ${GREEN}Test #9 Passed${NC}"
+    SCORE=$(($SCORE+4))
+else
+    echo -e "  ${RED}Failed${NC}"
+fi
+
+
+
+
+echo -e "\nTesting :: Correct sleep\n"
+if  [ $(grep -Pzo "sleep\s*\(.*6.*\)" main.cpp|wc -w) -ge 1 ]; then
+    echo -e "  ${GREEN}Test #10 Passed${NC}"
+    SCORE=$(($SCORE+2))
+else
+    echo -e "  ${RED}Failed${NC}"
+fi
+echo -e "\nTesting :: Correct WIFSIGNALED\n"
+if  [ $(grep -Pzo "WIFSIGNALED\s*\(.+\)" main.cpp|wc -w) -ge 1 ]; then
+    echo -e "  ${GREEN}Test #11 Passed${NC}"
+    SCORE=$(($SCORE+2))
+else
+    echo -e "  ${RED}Failed${NC}"
+fi
+
+echo -e "\nTesting :: Correct WIFEXITED\n"
+if  [ $(grep -Pzo "WIFEXITED\s*\(.+\)" main.cpp|wc -w) -ge 1 ]; then
+    echo -e "  ${GREEN}Test #12 Passed${NC}"
+    SCORE=$(($SCORE+2))
 else
     echo -e "  ${RED}Failed${NC}"
 fi
